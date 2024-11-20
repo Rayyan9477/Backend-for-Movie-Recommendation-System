13c6e51e8a34d1b9fa1b42f12671afa1c5468b03
safojan <jsafdar199@gmail.com>
2024-11-20 23:31:58 +0500

    fix the userprofile routes

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -7,7 +7,7 @@ const authMiddleware = (req, res, next) => {
   const token = req.header('x-auth-token');
 
   if (!token) {
-    return res.status(401).json({ msg: 'No token, authorization denied' });
+    return res.status(401).json({ msg: 'YYYNo token, authorization denied' });
   }
 
   try {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/authMiddleware');
+const auth = require('../middleware/auth');
 const User = require('../models/User');
 
 // @route   GET api/users/profile
@@ -12,7 +12,7 @@ const User = require('../models/User');
 
 router.get('/profile', auth, async (req, res) => {
   try {
-    const user = await findById(req.user.id).select('-password');
+    const user = await User.findById(req.user.id).select('-password');
     res.json(user);
   } catch (err) {
     console.error(err.message);
